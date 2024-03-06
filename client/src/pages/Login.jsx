1e27d803958f0af88f3cc8504520b1d727b8448f
Mr Groot <113226362+MrGrootx@users.noreply.github.com>
2024-03-06 16:18:58 +0530

    feat: proj files

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,8 +1,35 @@
+import { useState } from "react";
 
 const Login = () => {
+  const loginUser = (e) => {
+    e.preventDefault();
+  };
+  const [data, setData] = useState({
+    email: "",
+    password: "",
+  });
   return (
-    <div>Login</div>
-  )
-}
+    <div>
+      <form onSubmit={loginUser}>
+        <label htmlFor="">Email</label>
+        <input
+          type="email"
+          placeholder="Enter your email"
+          value={data.email}
+          onChange={(e) => setData({ ...data, email: e.target.value })}
+        />
 
-export default Login
+        <label htmlFor="">Password</label>
+        <input
+          type="password"
+          placeholder="Enter your password"
+          value={data.password}
+          onChange={(e) => setData({ ...data, password: e.target.value })}
+        />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,8 +1,46 @@
+import { useState } from "react";
 
 const Register = () => {
-  return (
-    <div>Register</div>
-  )
-}
+  const registerUser = (e) => {
+    e.preventDefault();
+  };
 
-export default Register
+  const [data, setData] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  return (
+    <div>
+      <form action="" onSubmit={registerUser}>
+        <label htmlFor="">Name</label>
+        <input
+          type="text"
+          placeholder="Enter your name"
+          value={data.name}
+          onChange={(e) => setData({ ...data, name: e.target.value })}
+        />
+
+        <label htmlFor="">Email</label>
+        <input
+          type="email"
+          placeholder="Enter your email"
+          value={data.email}
+          onChange={(e) => setData({ ...data, email: e.target.value })}
+        />
+
+        <label htmlFor="">Password</label>
+        <input
+          type="password"
+          placeholder="Enter your password"
+          value={data.password}
+          onChange={(e) => setData({ ...data, password: e.target.value })}
+        />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default Register;
