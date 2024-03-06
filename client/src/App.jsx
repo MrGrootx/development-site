9fa830b955890a2a245d870fce3d3453dfbe7c00
Mr Groot <113226362+MrGrootx@users.noreply.github.com>
2024-03-06 16:01:29 +0530

    prj: init

---
+++ client/src/App.jsx
@@ -0,0 +1,18 @@
+import "./index.css";
+
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </>
+  );
+};
+
+export default App;
